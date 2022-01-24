89c764fe9ebe0d3a8588c73c8e3c757bd1750790
箕輪　駿 <minowa@minowashun-no-MacBook-Air.local>
2022-01-24 22:34:25 +0900

    初コミット

---
+++ weatherApp/weatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by 箕輪　駿 on 2022/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
